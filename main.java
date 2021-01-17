c8dcb3337507786083a67a940fdd7c72ee73af53
Rubel hossain <42895415+rirubelhossain@users.noreply.github.com>
2021-01-17 16:55:32 +0600

    Add files via upload

---
+++ main.java
@@ -0,0 +1,11 @@
+package basic_java;
+
+public class main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello programmer \n");
+
+	}
+
+}
